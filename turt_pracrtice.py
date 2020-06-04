32e4d395f36f14376702d0cac52aca9b217658df
Dr Z <zamore@vt.edu>
2020-06-04 11:36:21 -0600

    First commit

---
+++ turt_pracrtice.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 11:14:59 2020
+
+@author: Dr. Z
+06/05 in-class drawing practice
+"""
+
+from turtle import * #import the library of commands that you'd like to use
+colormode(255)
+
+#Create a panel to draw on. 
+panel = Screen()
+panel.clear()
+w = 600 # width of panel
+h = 600 # height of panel
+panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
+#You can experiment by making it the size of your screen or super tiny!
+
+#Don't change this line (puts (0,0) at upper left corner)
+panel.setworldcoordinates(0, w, h, 0)
+
+#### LIST OF COLORS ####
+'''yellow, gold, orange, red, maroon, violet, magenta, 
+purple, navy, blue, skyblue, cyan, turquoise, lightgreen, 
+green, darkgreen, chocolate, brown, black, gray, white
+
+'''
+#=======Set your background color here======
+
+#panel.bgcolor("violet")
+
+#=======Create a turtle here======
+# Make your turtle any color you like!
+
+
+
+# Pick up the turtle pen and move it 300 pixels right and 300 pixels down
+
+
+
+# Set the pen down and draw a circle with a 100 pixel radius
+
+
+
+# Change the color
+
+
+
+# Rotate the turtle right 5 and draw another circle with a 100 pixel radius
+
+
+
+# Change the color a third time and repeat the rotation and circle
+
