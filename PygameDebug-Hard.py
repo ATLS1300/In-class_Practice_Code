5c2a5b5f01c7b600030b7595afad90ef871e73ed
Dr Z <1869730+sazamore@users.noreply.github.com>
2022-06-06 09:42:18 -0600

    Update and rename PygameDebug-What's_In_A_Name.py to PygameDebug-Hard.py

--- PygameDebug-Hard.py
+++ PygameDebug-Hard.py
@@ -5,25 +5,23 @@ Author: Dr. Z
 May 29, 2020
 '''
 import math, random
-from turtle import * #import the library of commands that you'd like to use
-colormode(255)
+import turtle #import the library of commands that you'd like to use
+turtle.colormode(255)
 
 #Create a panel to draw on. 
-panel = Screen()
+panel = turtle.Screen()
 panel.clear()
 w = 720 # width of panel
 h = 720 # height of panel
 panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
 #You can experiment by making it the size of your screen or super tiny!
 
-#Don't change this line (puts (0,0) at upper left corner)
-panel.setworldcoordinates(0, w, h, 0)
 panel.bgcolor('gray')
 
 Color = ['red','white']
 
-square = Turtle()
-square.color(color[0],color[2])
+square = turtle.Turtle()
+square.color(Color[-3],Color[-1])
 size = 200
 sides = 4
 angle = 360/4
@@ -33,9 +31,11 @@ square.goto(400.0,300)
 square.down()
 
 for i in range(300):
-    begin_fill()
+    turtle.begin_fill()
     for sides in range(4):
         square.forward(size)
         square.right(angle)
     square.rihgt(21)
-    end_fill()
+    square.end_fill()
+    
+ turtle.done()
